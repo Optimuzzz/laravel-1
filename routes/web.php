2e0da4284f302ee487673df6cfe00f0c81dcb065
Optimuzzz <paulo_crazy@hotmail.com>
2022-01-22 11:48:23 -0300

    atualizacao

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/produto', [ProdutosController::class, 'index']);
+Route::get('/produtos/{id}', [ProdutosController::class, 'show']);
